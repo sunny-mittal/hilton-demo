e06e12dad9670a17ceab86918937f5579f24a7e1
Sunny Mittal <sunnymittal2003@gmail.com>
2019-03-14 15:19:59 -0600

    Add testing frameworks and linting rules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  setupFilesAfterEnv: ['<rootDir>setup-tests.ts'],
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }

3302e4b9dd5c0446065db2951a81b54af13680dc
Sunny Mittal <sunnymittal2003@gmail.com>
2019-03-15 12:21:47 -0600

    Add App test

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class App extends Component {
+  public render() {
+    return (
+      <View>
+        <Text>Hilton Demo App</Text>
+      </View>
+    )
+  }
+}

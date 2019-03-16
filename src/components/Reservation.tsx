3073e028fde285b298803b0f20082a075e54c862
Sunny Mittal <sunnymittal2003@gmail.com>
2019-03-15 23:02:38 -0600

    Add basic reservation card

--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { Card } from 'react-native-elements'
 
 const Reservation = ({ name, hotelName }) => (
-  <View>
-    <Text>
-      Booking for {name} at {hotelName}
-    </Text>
-  </View>
+  <Card title={hotelName}>
+    <Text style={styles.text}>Guest: {name}</Text>
+    <Text style={styles.text}>In: </Text>
+    <Text style={styles.text}>Out: </Text>
+  </Card>
 )
+
+const styles = StyleSheet.create({
+  title: {},
+  text: {
+    lineHeight: 22
+  }
+})
 
 export default Reservation

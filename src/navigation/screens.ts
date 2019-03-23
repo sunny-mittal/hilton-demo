918a94bcb87fffe998b07045fe5460f17d21afc7
Sunny Mittal <sunnymittal2003@gmail.com>
2019-03-23 10:03:10 -0600

    Fix relative path issues

--- src/navigation/screens.ts
+++ src/navigation/screens.ts
@@ -1,7 +1,7 @@
 import { Navigation } from 'react-native-navigation'
 
-import CreateReservation from './components/CreateReservation'
-import Home from './components/HomeWrapper'
+import CreateReservation from '../components/CreateReservation'
+import Home from '../components/HomeWrapper'
 
 const registerScreens = () => {
   Navigation.registerComponent('Home', () => Home)
